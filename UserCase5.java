a4cad7b6bf9d302ef69cb6b3e67c7136bc1e7d03
pratap1804 <pratap.1804@gmail.com>
2021-06-24 08:02:53 +0530

    Ensuring winning position is exactly 100

---
+++ UserCase5.java
@@ -0,0 +1,46 @@
+public class SnakeAndLadderSimulator {
+ 	
+	public static int rollDice() {
+		return (int) Math.floor(Math.random() * 10) % 6 + 1;
+	}
+
+	public static int checkOption() {
+		return (int) Math.floor(Math.random() * 10) % 3 + 1;
+	}
+
+public static void main(String args[]) {
+		int playerPosition = 0;
+		int Winpoint = 100;
+		while(playerPosition < 100){
+		int random = rollDice();
+		int option = checkOption();
+		switch (option) {
+		case 1:
+			System.out.println("Player do not plays");
+			break;
+		case 2:
+			System.out.println("Player got the Ladder");
+			playerPosition += random;
+			if (playerPosition > 100) {
+				playerPosition -= random;
+			}
+			break;
+		case 3:
+			System.out.println("Oops! Player got the Snake");
+			playerPosition -= random;
+			
+			if (playerPosition < 0) {
+				playerPosition = 0;
+			} else{
+				playerPosition -= random;
+			}
+			break;
+		default:
+			System.out.println("Something Wrong");
+
+		}
+		System.out.println("Player position is:" + playerPosition);
+		System.out.println();
+	}
+    }
+}
